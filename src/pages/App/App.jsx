8251038568f4e0b0d9204dae36e6245b39bf2aca
CoreySumma <CoreySumma@gmail.com>
2023-07-11 14:16:14 -0500

    first commit/restructure

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../../images/zodiac.png" alt="" />
+        <p>
+          This is where the weather for the day is going to go.
+        </p>
+      <main>
+        <p className='App-main'>This will be where you see your Astrological prediction for the day.</p>
+      </main>
+      </header>
+    </div>
+  );
+}
+
