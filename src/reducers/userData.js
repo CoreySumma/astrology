403b47f5bd48b4763f255314a9c462e057e614dd
CoreySumma <CoreySumma@gmail.com>
2023-08-23 16:25:51 -0500

    updated console logs for reducers

--- src/reducers/userData.js
+++ src/reducers/userData.js
@@ -24,21 +24,25 @@ const userData = (state = intializeState, action) => {
         signData: action.payload,
       };
     case UPDATE_DATE:
+      console.log("Handling UPDATE_DATEaction", action);
       return {
         ...state,
         date: action.payload,
       };
     case UPDATE_TIME:
+      console.log("Handling UPDATE_TIME action", action);
       return {
         ...state,
         time: action.payload,
       };
     case UPDATE_TEMP:
+      console.log("Handling UPDATE_TEMP action", action);
       return {
         ...state,
         temp: action.payload,
       };
     case UPDATE_PREDICTION:
+      console.log("Handling UPDATE_PREDICTION action", action);
       return {
         ...state,
         prediction: action.payload,
