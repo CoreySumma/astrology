c57cb492250a9bd193f90b2b85d21cf7326578e7
CoreySumma <CoreySumma@gmail.com>
2023-09-12 18:36:05 -0500

    successfully rendered GPT response and tweaked prompt

--- src/components/DayAtAGlance/DayAtAGlance.jsx
+++ src/components/DayAtAGlance/DayAtAGlance.jsx
@@ -1,13 +1,16 @@
 import "./DayAtAGlance.css";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import gptApi  from "../../utilities/gpt-api";
+import gptApi from "../../utilities/gpt-api";
 
-export default function DayAtAGlance({sign, date, time, temp, location}) {
+export default function DayAtAGlance({ sign, date, time, temp, location }) {
   const dispatch = useDispatch();
+  const [prediction, setPrediction] = useState("");
   async function handleClick() {
     try {
       let result = await gptApi(sign, date, time, temp, location, dispatch);
       console.log("this is the result from gptApi", result);
+      setPrediction(result);
     } catch (error) {
       console.log("Error making call to gpt", error);
     }
@@ -16,9 +19,13 @@ export default function DayAtAGlance({sign, date, time, temp, location}) {
     <>
       <button onClick={handleClick}>Ask The Stars...</button>
       <div className="prediction-container">
-        <p className="prediction-text">
-          Please select your sign to see your prediction for today.
-        </p>
+        {prediction ? (
+          <p className="prediction-text">{prediction}</p>
+        ) : (
+          <p className="prediction-text">
+            Please select your sign to see your prediction for today.
+          </p>
+        )}
       </div>
     </>
   );
--- src/utilities/gpt-prompt.js
+++ src/utilities/gpt-prompt.js
@@ -11,9 +11,11 @@ Use all of this information to seem knowledgable as a fortune teller for the day
 Please make it as accurate as you can with what you know about this sign and the 
 day the user is requesting the prediction for. Use all of the information you have in your prediction.  Mention the day of the week, the date, the time, the temperature in a clever way.
 Example input: signData = Taurus, date = 2023-08-14, time = 1:00pm,temp = 100,location = New York
-Your Response: Today you are able to see the bigger picture.Even if
+Example Response: Today you are able to see the bigger picture.Even if
 you don't see what you want to see right now you will eventually.You don't have to
 be afraid of being wrong.Take it easy and look inward on this hot Monday afternoon. 
 Treat yourself by taking a dip at John Jay Pool on Cherokee PI.
+Now you try!
+Make sure to use Astrology terms and phrases in your prediction. Make sure to access all data you have for the current day and how that would effect each user.  Make sure to access all the data you have for Astrology and this time of year.
 `;
 }
