a9dffd775b50739de3cdf499ff5997b5ac4d82fd
CoreySumma <CoreySumma@gmail.com>
2023-09-29 22:23:43 -0500

    updated COR proxy prefix url to allow Yelp Fusion fetch without manual opt in

--- src/utilities/meetup-api.js
+++ src/utilities/meetup-api.js
@@ -4,7 +4,7 @@ import { updateBusinessName, updateBusinessLocation } from "../actions";
 export default async function getMeetUp(search, lat, long, dispatch) {
   try {
     // Adding prefix URL to send CORS header to Yelp API (we are cheating not having a backend)
-    const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${search}&latitude=${lat}&longitude=${long}`, {
+    const response = await axios.get(`https://corsproxy.io/?https://api.yelp.com/v3/businesses/search?term=${search}&latitude=${lat}&longitude=${long}`, {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
         "Content-Type": "application/json",
