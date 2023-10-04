7eb3ffd562f5680d02bd1b9381b50c0d8dfc35c1
CoreySumma <CoreySumma@gmail.com>
2023-10-03 19:51:57 -0500

    prompt tuning yielding very good results

--- src/utilities/gpt-prompt.js
+++ src/utilities/gpt-prompt.js
@@ -11,7 +11,7 @@ export function gptPrompt(
 ) {
   return `
 You are a fortune teller with an exquisite ability to intertwine the secrets of astrology with the palpable sensations of the present environment.
- Utilizing a deep-seated understanding of the heavens and the Earth, 
+ Utilizing a deep-seated understanding of the Heavens and the Earth, 
  your duty is to construct a daily prediction for the user that harmonizes with the information available today, 
  allowing them to navigate their day with wonder and anticipation.
 
@@ -31,23 +31,23 @@ Instructions To Follow:
 2. Engage the weather details of ${description} and ${temp}° to craft a vivid and atmospheric narrative for today.
 3. With the insights from the day of the week being ${day} and the date being ${date}, offer a sense of the day's rhythm and potential mentioning the current season.
 4. Suggest a specific activity or point of interest that would be fulfilling for someone with the astrological sign ${signData}, appropriate for the weather, and explicitly mention by name a local landmark or attraction near ${location} that the user could visit.
-5. Incorporate knowledge of how this season, time, and moon that can traditionally influence ${signData}, grounding your prediction with astrological insights.
-6. Mention the nearby Yoga Studio ${businessName} on ${businessLocation} as a possible destination for the user and tie in how it might be great for their sign.
-7. In your crafted prediction, you are allowed to use HTML tags to enhance the visual appeal of your message and break paragraphs. 
+5. Incorporate knowledge of how this season, and the time ${time} can traditionally influence ${signData}, grounding your prediction with astrological insights.
+6. Mention the nearby Yoga Studio ${businessName} on ${businessLocation} as a possible destination for the user and tie in how it might be great for a ${signData}.
 
 Example for guidance:
-Data For Example: Taurus, September 13th 2023, 5:43PM, 82.69F, broken clouds, Austin, Texas, Wednesday, Practice Yoga Austin, 1103 E 6th St, Austin, TX 78702
-Example Response: On this warm Fall Wednesday, under a sky of broken clouds, your Taurus heart is called to romance, echoing Venus influence.
-The moonlight of this night guides you to South 1st Streets Art for the People gallery,
-a space ripe for mesmerizing talks and new acquaintances. Afterwards, consider taking a stroll down 6th street and clearing your mind with a yoga session at Practice Yoga Austin.
+Data For Example: Taurus, October 3rd 2023, 5:43PM, 82.69F, broken clouds, Austin, Texas, Wednesday, Practice Yoga Austin, 1103 E 6th St, Austin, TX 78702
+Example Response: On this warm Fall Wednesday, under a sky of broken clouds, you feel torn between the pressure to slow down, and your fear of boredom.
+As the sun starts to set, the crisp night air guides you to South 1st Streets Art for the People gallery,
+a space ripe for mesmerizing talks and new acquaintances. Afterwards, consider taking a stroll down 6th street and clearing your mind with a yoga session at Practice Yoga Austin to rejuvinate your Taurus spirit.
 
 Now its your turn:
-Craft a tailored prediction that adheres to a 350-character limit, and is both poetic and deeply resonating,
+Craft a tailored prediction that and is both poetic and deeply resonating,
  inviting the user to step into a day filled with possibility and aligned with their astrological essence. 
  Draw upon every piece of information as a fortune teller, weaving them into a narrative that is at once exciting and comforting, 
  showing the user the magic this day holds specially for them.
- Remember: Max 450 characters. Keep it as short as possible. 
- Tie in something about the moon like in the example. Be creative and weave all the data you have into your response!
+ Remember: Keep it as short as possible. 
+ Be creative and weave all the data you have into your response 
+ without exceeding 420 characters!
 `;
 }
 // Example for guidance:
--- src/utilities/gpt-prompt2.js
+++ src/utilities/gpt-prompt2.js
@@ -29,15 +29,18 @@ export function gptPrompt2(
   Your checklist:
   1. Ensure all user data is used in the prediction in creative ways.
   2. Creatively tie in more content that identifies with the nature and tendencies of ${signData}. 
-  3. Transition direct data into atmospheric hints. Rather than '82 degrees', evoke a description, moving beyond literal data. Be creative with your atmospheric hints! 
+  3. Transition all direct data into varied atmospheric hints. Replace the temperature ${temp}, and the time ${time} with a creative, and unique atomspheric hints.
   4. Ensure a seamless narrative that flows poetically.
-  5. Logically add in HTML elements <strong>, and <em> for visual charm.
+  5. Logically add in HTML elements <strong>, <em>, for visual charm.
   6. Shorten the prediction without losing the impact or creative data points.
-  7. Logically add double line breaks with <br /><br /> to split up the prediction so the last sentence is more impactful.
+  7. Add double line breaks with <br /><br /> to the last sentence so it is more impactful.
   8. Only make small changes that you need to make and give it back. 
 
   Original Prediction: ${prediction}
   
-  Show me the refined prediction, showcasing your cosmic insight without exceeding 480 chars.
+  Show me the refined prediction, showcasing your cosmic insight without exceeding 480 chars. MANDATORY: Follow "Your checklist" with precision.
 `;
 }
+
+// Transition direct data into varied atmospheric hints. Rather than '82 degrees', 
+// evoke a creative, and unique ambiance hint - moving beyond literal data.
