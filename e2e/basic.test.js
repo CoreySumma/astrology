17fdd621bbcc990468259ff1b2821bbeda2746b5
CoreySumma <CoreySumma@gmail.com>
2024-10-18 18:14:38 -0500

    add playwright github actions workflow

---
+++ e2e/basic.test.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line
+import { test } from "@playwright/test";
+
+
+test.describe("Loading Location", () => {
+  test("should display the swiper and grant location permissions", async ({
+    browser,
+  }) => {
+    const context = await browser.newContext({
+      permissions: ['geolocation'],
+      geolocation: { latitude: 50.8551729, longitude: 4.340312 },
+    });
+    const page = await context.newPage();
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector("text=Loading Location");
+  });
+});
