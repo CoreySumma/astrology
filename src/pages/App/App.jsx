4dfab7caa436d04d9a741961e111e4acbcbb9124
CoreySumma <CoreySumma@gmail.com>
2024-10-26 16:15:51 -0500

    undo

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
   }, [locationFetched]);
 
   return (
-    <div className={`App ${fade ? '' : 'no-scroll'}`}>
+    <div className="App">
       <div className="video-background">
         <video autoPlay playsInline muted loop preload="auto">
           <source
